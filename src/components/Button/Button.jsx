378b7e2499a6ec7caa5be9de1f749ead7d07f34c
rohitsinghwho <rohiisingh8655@gmail.com>
2025-07-20 12:19:13 +0530

    button updated

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "./Button.module.css"
 const Button = ({children}) => {
   return (
-    <div className={styles.button}>{children}</div>
+    <button className={styles.button}>{children}</button>
   )
 }
 
